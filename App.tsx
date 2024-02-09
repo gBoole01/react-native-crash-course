dc078a8178f472c9f9f672c81b812ac61ea511ac
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2024-02-09 11:18:48 +0100

    feat: add DetailScreen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import DetailScreen from './components/DetailScreen';
 import HomeScreen from './components/HomeScreen';
 
 const Stack = createNativeStackNavigator();
@@ -9,7 +10,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Pokedex" component={HomeScreen} />
-        <Stack.Screen name="Pokemon" component={HomeScreen} />
+        <Stack.Screen name="Pokemon" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
     );
